794ac0712234edc41eae059ef189b1c73f7ee101
Aldrin Thivyanathan <aldrin.a.thivyanathan@pwc.com>
2018-01-16 10:55:12 +0530

    Initial Commit

---
+++ DemoXamarin/DemoXamarinPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace DemoXamarin
+{
+    public partial class DemoXamarinPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Message", "You have tapped the Button!", "OK");
+        }
+
+        public DemoXamarinPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
